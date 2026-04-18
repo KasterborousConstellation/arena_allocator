a2110f0f2c77a373f1558a1305749b4a8ed94d32
KasterborousConstellation <paul.andrieu28@gmail.com>
2026-04-18 21:55:53 +0200

    better handling of invalid blocks and allocation

--- example.c
+++ example.c
@@ -1,34 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 #define KTBS_ALLOCATOR_IMPLEMENTATION
 #include "ktbs_allocator.h"
+#define N 1000
 int main(){
-    ktbs_allocator* allocator = ktbs_allocator_create(1 << 18,"Kappa");
+    ktbs_allocator* allocator = ktbs_allocator_create(1 << 29,"Kappa");
     if(allocator == NULL){
         fprintf(stderr, "Failed to create allocator\n");
         return 1;
     }
-    printf("Allocator created with size: %zu\n", size_of_allocator(allocator));
-    ktbs_block block2 = ktbs_allocator_alloc(allocator, 130000);
-    show_occupancy(allocator->occupancy);
-    ktbs_block block3 = ktbs_allocator_alloc(allocator, 130000);
-    show_occupancy(allocator->occupancy);
-    ktbs_block block4 = ktbs_allocator_alloc(allocator, 130000);
-    show_occupancy(allocator->occupancy);
-    ktbs_block block5 = ktbs_allocator_alloc(allocator, 263000);
-    show_occupancy(allocator->occupancy);
-    ktbs_block block6 = ktbs_allocator_alloc(allocator, 130000);
-    show_occupancy(allocator->occupancy);
-    block_info(block2);
-    block_info(block3);
-    block_info(block4);
-    block_info(block5);
-    block_info(block6);
-    free_block(block3);
-    printf("Freed block 3\n");
-    
-    
+    allocator_info(allocator);
+    ktbs_block block = ktbs_allocator_alloc(allocator, sizeof(ktbs_block)*N);
+    ktbs_block* block_array = (ktbs_block*) block.ptr;
+    time_t start = time(NULL);
+    for(int i=0; i<N; i++){
+        block_array[i] = ktbs_allocator_alloc(allocator, 1 << 16);
+    }
+    for(int i=0; i<N/2; i++){
+        free_block(block_array[i]);
+    }
+    for(int i=0;i<N/2; i++){
+        block_array[i] = ktbs_allocator_alloc(allocator, 1 << 16);
+    }
+    for(int i=N/2; i<N; i++){
+        free_block(block_array[i]);
+    }
+    time_t end = time(NULL);
+    printf("Deallocation time: %ld seconds\n", end - start);
     ktbs_allocator_destroy(allocator);
     printf("Allocator destroyed\n");
     return 0;
--- ktbs_allocator.h
+++ ktbs_allocator.h
@@ -5,6 +5,7 @@
 #include <errno.h>
 #define KTBS_ALLOCATOR_BIT_BLOCK_SIZE 16
 struct ktbs_occupancy{
+    size_t log_biggest_free;
     bool occupied;
     bool full;
     struct ktbs_occupancy* left;
@@ -32,6 +33,7 @@ typedef struct ktbs_block ktbs_block;
 ktbs_block ktbs_allocator_alloc(ktbs_allocator* allocator, size_t size);
 void free_block(ktbs_block block);
 void block_info(ktbs_block block);
+void allocator_info(ktbs_allocator* allocator);
 #ifdef KTBS_ALLOCATOR_IMPLEMENTATION
 #define isleaf(occupancy) (occupancy->left == NULL && occupancy->right == NULL)
 #define log_size_of(size) (max(leftbit(size) +1, KTBS_ALLOCATOR_BIT_BLOCK_SIZE))
@@ -65,8 +67,15 @@ int leftbit(size_t n){
 void propagate_up(ktbs_occupancy* occupancy){
     if(occupancy == NULL) return;
     if(!isleaf(occupancy)){
+        if(occupancy->left->occupied || occupancy->right->occupied){
+            occupancy->log_biggest_free = max(occupancy->left->log_biggest_free, occupancy->right->log_biggest_free);
+        }else{
+            occupancy->log_biggest_free = occupancy->log_size;
+        }
+        occupancy->full = occupancy->left->full && occupancy->right->full;
         occupancy->occupied = occupancy->left->occupied || occupancy->right->occupied;
-        occupancy->full = occupancy->left->full && occupancy->right->full;  
+    }else{
+        occupancy->log_biggest_free = occupancy->occupied ? 0 : occupancy->log_size;
     }
     propagate_up(occupancy->parent);
 }
@@ -82,6 +91,7 @@ ktbs_occupancy* create_occupancy(size_t log_size,size_t offset){
     occupancy->occupied = false;
     occupancy->full = false;
     occupancy->parent = NULL;
+    occupancy->log_biggest_free = log_size;
     if(log_size == KTBS_ALLOCATOR_BIT_BLOCK_SIZE){
         occupancy->left = NULL;
         occupancy->right = NULL;
@@ -112,26 +122,48 @@ ktbs_allocator* ktbs_allocator_create(size_t size, const char* name){
 size_t size_of_allocator(ktbs_allocator* allocator){
     return 1 << allocator->log_size;
 }
+
 void propagate_down(ktbs_occupancy* occupancy, bool occupied){
     if(occupancy == NULL)return;
     occupancy->occupied = occupied;
     occupancy->full = occupied;
+    if(occupied){
+        occupancy->log_biggest_free = 0;
+    }else{
+        occupancy->log_biggest_free = occupancy->log_size;
+    }
     propagate_down(occupancy->left, occupied);
     propagate_down(occupancy->right, occupied);
 }
+/*
+    Precondition : log_size >= KTBS_ALLOCATOR_BIT_BLOCK_SIZE
+*/
 ktbs_block alloc_block_aux(ktbs_allocator* allocator, ktbs_occupancy* occupancy, size_t log_size){
-    if(log_size ==occupancy->log_size && !(occupancy->occupied)){
+    // this current part of the tree can perfectly fit the block
+    if((log_size ==occupancy->log_size) && !(occupancy->occupied)){
+        //We choose this part of the tree
         occupancy->occupied = true;
         propagate_down(occupancy, true);
         propagate_up(occupancy);
         return (ktbs_block){1 << occupancy->log_size, (void*)(allocator->memory+occupancy->offset),allocator};
     }
+    // if this part of the tree can't fit the block, return an empty block
+    if(log_size > occupancy->log_biggest_free){
+        return (ktbs_block){0,NULL,NULL};
+    }
+    //this part of the tree can fit the block but not perfectly we need to go deeper in the tree
     if(log_size < occupancy->log_size){
-        ktbs_block block_left = alloc_block_aux(allocator, occupancy->left, log_size);
-        if(block_left.size !=0) return block_left;
-        ktbs_block val = alloc_block_aux(allocator, occupancy->right, log_size);
-
-        return val;
+        size_t log_biggest_free_left = occupancy->left ? occupancy->left->log_biggest_free : 0;
+        if(log_biggest_free_left >= log_size){
+            //we can fit the block in the left part of the tree
+            ktbs_block block_left = alloc_block_aux(allocator, occupancy->left, log_size);
+            if(block_left.size !=0) return block_left;
+        }
+        else{
+            //we can fit the block in the right part of the tree
+             ktbs_block block_right = alloc_block_aux(allocator, occupancy->right, log_size);
+            if(block_right.size !=0) return block_right;
+        }
     }
 
     return  (ktbs_block){0,NULL,NULL};
@@ -142,7 +174,7 @@ ktbs_block ktbs_allocator_alloc(ktbs_allocator* allocator, size_t size){
         errno = EINVAL;
         fprintf(stderr, "Invalid size to allocate: %zu\n", size);
     }else{
-        size_t log_size = log_size_of(size);
+        size_t log_size = max(log_size_of(size), KTBS_ALLOCATOR_BIT_BLOCK_SIZE);
         block = alloc_block_aux(allocator, allocator->occupancy, log_size);
     }
     return block;
@@ -153,7 +185,7 @@ void show_occupancy_aux(ktbs_occupancy* occupancy, int depth){
     for(int i=0; i<depth; i++) printf("  ");
     const char* color = occupancy->full ? "\033[31m" : "\033[32m";
     
-    printf("-%sOffset: %zu, Log Size: %zu, Full: %s, Occupied: %s%s\n",color, occupancy->offset, occupancy->log_size, occupancy->full ? "Yes" : "No", occupancy->occupied ? "Yes" : "No", reset_color);
+    printf("-%sOffset: %zu, Log Size: %zu, Full: %s, Occupied: %s, Log Biggest Free: %zu%s\n",color, occupancy->offset, occupancy->log_size, occupancy->full ? "Yes" : "No", occupancy->occupied ? "Yes" : "No", occupancy->log_biggest_free , reset_color);
     show_occupancy_aux(occupancy->left, depth +1);
     show_occupancy_aux(occupancy->right, depth +1);
 }
@@ -201,5 +233,18 @@ void block_info(ktbs_block block){
         return;
     }
     printf("Block Info \n  Size: %zu\n  Ptr: %p\n  Allocator Name: %s\n", block.size, block.ptr, block.allocator->name);
+}
+void allocator_info(ktbs_allocator* allocator){
+    char print_size[256];
+    // Convert size to human readable format
+    size_t size = size_of_allocator(allocator);
+    const char* suffixes[] = {"B", "KB", "MB", "GB", "TB"};
+    int suffix_index = 0;
+    while(size >= 1024 && suffix_index < 4){
+        size /= 1024;
+        suffix_index++;
+    }
+    snprintf(print_size, 256, "%zu %s", size, suffixes[suffix_index]);
+    printf("Allocator Info \n Name: %s\n Total Size: %s\n", allocator->name, print_size);
 }
 #endif // KTBS_ALLOCATOR_IMPLEMENTATION
